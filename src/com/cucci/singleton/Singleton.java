2ac4b4ce3fff9ad2c7df1b4c72487d8f99991686
shenyw <shenyw@citycloud.com.cn>
2019-01-31 10:53:12 +0800

    feat: 单例模式

---
+++ src/com/cucci/singleton/Singleton.java
@@ -0,0 +1,20 @@
+package com.cucci.singleton;
+
+public class Singleton {
+
+    private static volatile Singleton singleton;
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+
+    private Singleton() {
+    }
+}
---
+++ src/com/cucci/singleton/SingletonTest.java
@@ -0,0 +1,12 @@
+package com.cucci.singleton;
+
+/**
+ * 单例测试
+ *
+ * @author shenyw
+ **/
+public class SingletonTest {
+
+    public static void main(String[] args) {
+    }
+}
